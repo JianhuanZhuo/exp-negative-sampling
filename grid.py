a2b5d4c9b1af0eeaebf8cce7f31df88ccb59bb91
tom <me@zhuojianhuan.cn>
2021-06-13 00:41:39 +0800

    grid 参数调整

--- grid.py
+++ grid.py
@@ -11,8 +11,9 @@ if __name__ == '__main__':
     exp_config = config.load_specific_config("config.yaml")
 
     grid = {
-        "DataLoader/batch_size": [1024, 256, 32],  # nbatch 先用大批次加速训练，找到好的参数后再用小的吧
-        "optimizer/lr": [0.01, 0.03, 0.1, 0.3],  # learning_rate
+        "DataLoader/batch_size": [1024],  # nbatch 先用大批次加速训练，找到好的参数后再用小的吧
+        "optimizer/lr": [0.1, 0.3],  # learning_rate
+        "optimizer/weight_decay": [0.1, 1, 10],  # learning_rate
     }
 
     exp_config['log_folder'] = 'grid'

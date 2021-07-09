d9a0cc73518318101ec74d3332b16a0345da832e
tom <me@zhuojianhuan.cn>
2021-07-09 10:28:54 +0800

    使用有 softw 的搜索

--- dataset/SRNS_ml1m.py
+++ dataset/SRNS_ml1m.py
@@ -16,7 +16,11 @@ class SRNSML1MDataset(Dataset):
         self.test_data = pickle.load(open(os.path.join(folder, 'test.pkl'), 'rb'))
         self.test_data_neg = pickle.load(open(os.path.join(folder, 'test_neg.pkl'), 'rb'))
 
-        self.size = config['sample_group_size']
+        self.size_pos = self.size_neg = config['sample_group_size']
+        if 'sample_group_size_pos' in config:
+            self.size_pos = config['sample_group_size_pos']
+        if 'sample_group_size_neg' in config:
+            self.size_neg = config['sample_group_size_neg']
 
         self.num_user = max(np.max(self.train_data[:, 0]), np.max(self.test_data[:, 0])) + 1
         self.num_item = max(np.max(self.train_data[:, 1]), np.max(self.test_data[:, 1])) + 1
@@ -37,7 +41,9 @@ class SRNSML1MDataset(Dataset):
             for u, iss in self.uis.items()
         }
 
-        self.index_by_user = 'dataset/index_by_user' in config and config['dataset/index_by_user']
+        self.index_by_user = config.get_or_default('dataset/index_by_user', False)
+
+        self.ui_loss_record = torch.ones([self.num_user, self.num_item]) * 100
 
     def __len__(self):
         if self.index_by_user:
@@ -52,26 +58,14 @@ class SRNSML1MDataset(Dataset):
             user = self.train_data[index][0]
 
         if self.config.get_or_default("dataset/noise", False):
-            nop = self.config.get_or_default("dataset/noise_p")
-            neg_n = int(max(1, nop*self.size))
-            pos_n = self.size - neg_n
-            # positives = random.choices(self.ui_list[user], k=pos_n)
-            # for _ in range(neg_n):
-            #     neg = np.random.randint(0, self.num_item)
-            #     while neg in self.uis[user]:
-            #         neg = np.random.randint(0, self.num_item)
-            #     positives.append(neg)
+            nop = self.config.get_or_default("dataset/noise_p", 0.0)
+            neg_n = int(max(1, nop*self.size_pos))
+            pos_n = self.size_pos - neg_n
             positives = random.choices(self.ui_list[user], k=pos_n) + random.choices(self.u_negs[user], k=neg_n)
             random.shuffle(positives)
         else:
-            positives = random.choices(self.ui_list[user], k=self.size)
+            positives = random.choices(self.ui_list[user], k=self.size_pos)
 
-        # negatives = []
-        # for _ in range(self.size):
-        #     neg = np.random.randint(0, self.num_item)
-        #     while neg in self.uis[user]:
-        #         neg = np.random.randint(0, self.num_item)
-        #     negatives.append(neg)
-        negatives = random.choices(self.u_negs[user], k=self.size)
+        negatives = random.choices(self.u_negs[user], k=self.size_neg)
 
         return torch.tensor(user), torch.tensor(positives), torch.tensor(negatives)
